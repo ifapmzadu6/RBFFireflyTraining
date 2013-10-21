cc1732c87a87a9ef3285cbdb77d3c85c3db7156f
Keisuke Karijuku <KeisukeKarijuku@Keisuke-no-MacBook-Air.local>
2013-10-21 16:02:26 +0900

    Initial Commit

---
+++ FireflyProject/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FireflyProject
+//
+//  Created by Keisuke Karijuku on 2013/10/21.
+//  Copyright (c) 2013年 Keisuke Karijuku. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
