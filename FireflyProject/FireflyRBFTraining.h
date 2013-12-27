6782c95846462856fce1250fb0cd91c39b65eefb
Keisuke Karijuku <KeisukeKarijuku@Keisuke-no-MacBook-Air.local>
2013-12-27 21:23:44 +0900

    バージョン１．０

--- FireflyProject/FireflyRBFTraining.h
+++ FireflyProject/FireflyRBFTraining.h
@@ -38,16 +38,12 @@ public:
         //outputのバイアス
         std::vector<double> biases;
         
-    public:
+        //適応度
+        double fitness;
+        
         Firefly(int dim, int dataCount, int rbfCount, const Eigen::MatrixXd &weights, const std::vector<double> spreads, const std::vector<Eigen::VectorXd> &centerVector, const std::vector<double> biases)
-        {
-            this->dim = dim;
-            this->dataCount = dataCount;
-            this->rbfCount = rbfCount;
-            this->weights = weights;
-            this->spreads = spreads;
-            this->centerVector = centerVector;
-            this->biases = biases;
+        : dim(dim), dataCount(dataCount), rbfCount(rbfCount), weights(weights), spreads(spreads), centerVector(centerVector), biases(biases) {
+            
         }
         
         double function(const Eigen::VectorXd &X, int rbfIndex) const {
@@ -87,7 +83,7 @@ public:
     
 private:
     //最大試行回数
-    int iteration;
+    int maxGeneration;
     //最大誤差
     double eps;
     //教師信号input,outputの次元
@@ -109,37 +105,23 @@ private:
     double gumma;
     
     //乱数の影響力を決める
-    double sigma;
+    double alpha;
     
     //NS-FAのfireflyの関係図のフラグ(fireflyCount, fireflyCount),Network-Structured Firefly Algorithmで使用
     std::vector<std::vector<bool>> connection;
     
-    //一番結果のいいfireflyのindex
-    int bestFireflyIndex;
-    
     std::random_device random;
-    
     std::mt19937 mt;
     std::uniform_real_distribution<double> score;
     
 public:
-    FireflyRBFTraining(int dim, int dataCount, int rbfCount, int fireflyCount, double attractiveness, double gumma, int iteration) {
-        this->iteration = iteration;
-        this->dim = dim;
-        this->dataCount = dataCount;
-        this->rbfCount = rbfCount;
-        this->fireflyCount = fireflyCount;
-        this->attractiveness = attractiveness;
+    FireflyRBFTraining(int dim, int dataCount, int rbfCount, int fireflyCount, double attractiveness, double gumma, int maxGeneration)
+    : dim(dim), dataCount(dataCount), rbfCount(rbfCount), fireflyCount(fireflyCount), attractiveness(attractiveness), gumma(gumma), maxGeneration(maxGeneration) {
         this->attractivenessMin = 0.2;
-        this->gumma = gumma;
-        this->bestFireflyIndex = 0;
-        this->sigma = 0.5;
         this->eps = 10e-6;
+        this->alpha = gumma;
         this->mt = std::mt19937(random());
-        this->score = std::uniform_real_distribution<double>(0.0, 1.0f);
-        
-        std::mt19937 mt(random());
-        std::uniform_real_distribution<double> score(0.0, 1.0);
+        this->score = std::uniform_real_distribution<double>(0.0, 1.0);
         
         for (int i = 0; i < fireflyCount; i++) {
             Eigen::MatrixXd weights = Eigen::MatrixXd::Random(rbfCount, dim);
@@ -163,7 +145,7 @@ public:
         for (int i = 0; i < fireflyCount; i++) {
             std::vector<bool> tmpVector(fireflyCount);
             for (int j = 0; j < fireflyCount; j++) {
-                tmpVector[j] = true;
+                tmpVector[j] = false;
             }
             connection.push_back(tmpVector);
         }
@@ -171,78 +153,92 @@ public:
     
     //教師信号入力input、教師信号出力output
     void training(const std::vector<Eigen::VectorXd> &input, const std::vector<Eigen::VectorXd> &output) {
-        std::cout << "Start training!" << std::endl;
+        std::cout << "-------Firefly Algorithm-------" << std::endl;
+        std::cout << "Dimention = " << dim << " , RBFCount = " << rbfCount << " , FireflyCount = " << fireflyCount << " , MaxGeneration = " << maxGeneration << std::endl;
+        std::cout << "[Start training!]" << std::endl;
         
+//        double cp;
+        //fitnessの計算
         std::vector<Eigen::VectorXd> tmpInput(dataCount);
-        
-        int iMax = 0;
-        double tmpf = 0.0;
-        int iter = 0;
-        double cp;
-        double sigmaT = sigma * (1.0 - pow((10e-4 / 0.9), (1.0 / iteration)));
-        double rij, beta;
-        //fitnessの値のキャッシュ
-        std::vector<double> fitnesses(fireflyCount);
         for (int i = 0; i < fireflyCount; i++) {
-            for (int k = 0; k < dataCount; k++) {
-                tmpInput[k] = fireflies[i].output(input[k]);
-            }
-            fitnesses[i] = fitness(tmpInput, output);
+            for (int k = 0; k < dataCount; k++) tmpInput[k] = fireflies[i].output(input[k]);
+            fireflies[i].fitness = fitness(tmpInput, output);
         }
+        std::sort(fireflies.begin(), fireflies.end(), compare);
         
-        while (iter < iteration) {
-            cp = (double)iter / iteration;
-            for (int i = 0; i < fireflyCount; i++) {
-                for (int j = 0; j < fireflyCount; j++) {
-                    //もしfitnessが良ければそのfireflyに近づく
-                    if (fitnesses[i] < fitnesses[j]) {
-                        //下のコメントアウトを外せばNetwork-Structured Firefly Algorithm
-//                        if (connection[i][j]) {
-                            rij = distanceBetweenTwoFireflies(fireflies[i], fireflies[j]);
-                            beta = (attractiveness - attractivenessMin) * exp(-gumma * rij) + attractivenessMin;
-                            moveFirefly(fireflies[i], fireflies[j], beta, sigmaT);
-                            //移動することでfitnessの値が変わるので再計算
-                            for (int k = 0; k < dataCount; k++) {
-                                tmpInput[k] = fireflies[i].output(input[k]);
-                            }
-                            fitnesses[i] = fitness(tmpInput, output);
-//                        }
-//                        else if (score(mt) <= cp)
-//                            connection[i][j] = true;
-                    }
-//                    else if(i != iMax)
-//                        if (score(mt) <= cp)
-//                            connection[i][j] = false;
-                }
-//                std::cout << (double)i / fireflyCount * 100 << "%...done." << std::endl;
-            }
-            //一番いい結果のindexを検索
-            tmpf = 0.0;
-            iMax = 0;
-            for (int i = 0; i < fireflyCount; i++) {
-                if (fitnesses[i] == 0.0) {
-                    for (int j = 0; j < dataCount; j++) {
-                        tmpInput[j] = fireflies[i].output(input[j]);
-                    }
-                    fitnesses[i] = fitness(tmpInput, output);
-                }
-                if (tmpf < fitnesses[i]) {
-                    tmpf = fitnesses[i];
-                    iMax = i;
-                }
-            }
-            //一番いいfireflyはランダムに移動
-            randomlyWalk(fireflies[iMax], sigmaT);
-            //キャッシュから削除
-            fitnesses[iMax] = 0.0;
+        int iter = 0;
+        double delta = 1.0-pow((pow(10.0, -4.0)/0.9), 1.0/maxGeneration);
+        
+        while (iter < maxGeneration) {
+            alpha = (1-delta)*alpha;
             
-            std::cout << "iter = " << iter << ", maxI = " << iMax << ", maxFitness = " << tmpf << std::endl;
-//            this->displayFirefly(iMax);
+            std::vector<Firefly> tmpFireflies;
+            std::copy(fireflies.begin(), fireflies.end(), std::back_inserter(tmpFireflies));
             
+            auto iterI = fireflies.begin();
+            while (iterI != fireflies.end()) {
+                auto iterJ = tmpFireflies.begin();
+                while (iterJ != tmpFireflies.end()) {
+                    if ((*iterI).fitness < (*iterJ).fitness) {
+                        moveFirefly(*iterI, *iterJ);
+                    }
+                    iterJ++;
+                }
+                
+                auto tmpInputIter = tmpInput.begin();
+                auto inputIter = input.begin();
+                while (tmpInputIter != tmpInput.end()) {
+                    *tmpInputIter = (*iterI).output(*inputIter);
+                    tmpInputIter++;
+                    inputIter++;
+                }
+                (*iterI).fitness = fitness(tmpInput, output);
+                iterI++;
+            }
+            
+            std::sort(fireflies.begin(), fireflies.end(), compare);
+            
+//            //一番いいfireflyはランダムに移動
+            Firefly bestFirefly = fireflies[0];
+            randomlyWalk(bestFirefly);
+            auto tmpInputIter = tmpInput.begin();
+            auto inputIter = input.begin();
+            while (tmpInputIter != tmpInput.end()) {
+                *tmpInputIter = bestFirefly.output(*inputIter);
+                tmpInputIter++;
+                inputIter++;
+            }
+            bestFirefly.fitness = fitness(tmpInput, output);
+            std::sort(fireflies.begin(), fireflies.end(), compare);
+            
+            std::cout << "iter = " << iter << ", maxFitness = " << fireflies[0].fitness << std::endl;
             iter++;
         }
-        //一番結果がいいindexを保存
-        bestFireflyIndex = iMax;
+    }
+    
+    void trainingNS () {
+        //            cp = (double)iter / maxGeneration;
+        
+        //            for (int i = 0; i < fireflyCount; i++) {
+        //                for (int j = 0; j < fireflyCount; j++) {
+        //                    //もしfitnessが良ければそのfireflyに近づく
+        //                    if (tmpFireflies[i].fitness < tmpFireflies[j].fitness) {
+        //                        //下のコメントアウトを外せばNetwork-Structured Firefly Algorithm
+        ////                        if (connection[i][j]) {
+        //                            //fireflyを近づける
+        //                            moveFirefly(fireflies[i], tmpFireflies[j]);
+        ////                        }
+        ////                        else if (score(mt) <= cp) connection[i][j] = true;
+        //                    }
+        ////                    else if(i != iMax) if (score(mt) <= cp) connection[i][j] = false;
+        //                }
+        ////                fitnesses[i] = fitness(tmpInput, output);
+        ////                std::cout << (double)i / fireflyCount * 100 << "%...done." << std::endl;
+        //            }
+    }
+    
+    static bool compare(const Firefly& obj1, const Firefly& obj2) {
+        return obj1.fitness < obj2.fitness;
     }
     
     //Fireflyの出力d、教師信号の出力o
@@ -253,15 +249,16 @@ public:
     //Fireflyの出力d、教師信号の出力o
     double mse(const std::vector<Eigen::VectorXd> &d, const std::vector<Eigen::VectorXd> &o) const {
         double mse = 0.0;
-        
-        for (int i = 0; i < dataCount; i++) {
+        std::vector<Eigen::VectorXd>::const_iterator dIter = d.begin();
+        std::vector<Eigen::VectorXd>::const_iterator oIter = o.begin();
+        while (dIter != d.end()) {
             for (int j = 0; j < dim; j++) {
-                mse += (d[i](j) - o[i](j)) * (d[i](j) - o[i](j));
+                mse += ((*dIter)(j) - (*oIter)(j)) * ((*dIter)(j) - (*oIter)(j));
             }
+            dIter++;
+            oIter++;
         }
-        
         mse /= dataCount;
-        
         return mse;
     }
     
@@ -276,64 +273,159 @@ public:
                 radius += tmp * tmp;
             }
             
-            tmp = (firefly1.spreads[i] - firefly2.spreads[i]);
-            radius += tmp * tmp;
-            
             for (int j = 0; j < dim; j++) {
                 tmp = firefly1.centerVector[i](j) - firefly2.centerVector[i](j);
                 radius += tmp * tmp;
             }
         }
         
-        for (int i = 0; i < dim; i++) {
-            tmp = (firefly1.biases[i] - firefly2.biases[i]);
-            radius += tmp * tmp;
+        auto spreadsIter1 = firefly1.spreads.begin();
+        auto spreadsIter2 = firefly2.spreads.begin();
+        while (spreadsIter1 != firefly1.spreads.end()) {
+            radius += (spreadsIter1 - spreadsIter2) * (spreadsIter1 - spreadsIter2);
+            spreadsIter1++;
+            spreadsIter2++;
         }
         
-        return radius;
+        auto biasesIter1 = firefly1.biases.begin();
+        auto biasesIter2 = firefly2.biases.begin();
+        while (biasesIter1 != firefly1.biases.end()) {
+            radius += (biasesIter1 - biasesIter2) * (biasesIter1 - biasesIter2);
+            biasesIter1++;
+            biasesIter2++;
+        }
+        
+        return sqrt(radius);
     }
     
     //firefly <- (1-beta)*firefly + beta*firefly + (rand-1/2)
-    void moveFirefly(Firefly &firefly, const Firefly &destinationFirefly, double beta, double sigma) {
+    void moveFirefly(Firefly &firefly, const Firefly &destinationFirefly) {
+        double rij = distanceBetweenTwoFireflies(firefly, destinationFirefly);
+        double beta = (attractiveness - attractivenessMin) * exp(-gumma * pow(rij, 2.0)) + attractivenessMin;
+        
         for (int i = 0; i < rbfCount; i++) {
             for (int j = 0; j < dim; j++) {
-                firefly.weights(i, j) = (1 - beta) * firefly.weights(i, j) + beta * destinationFirefly.weights(i, j) + sigma * (score(mt) - 0.5);
+                firefly.weights(i, j) = (1 - beta) * firefly.weights(i, j) + beta * destinationFirefly.weights(i, j) + alpha * (score(mt) - 0.5) * 2;
             }
             
-            firefly.spreads[i] = (1 - beta) * firefly.spreads[i] + beta * destinationFirefly.spreads[i] + sigma * (score(mt) - 0.5);
+            firefly.spreads[i] = (1 - beta) * firefly.spreads[i] + beta * destinationFirefly.spreads[i] + alpha * (score(mt) - 0.5) * 2;
             
             for (int j = 0; j < dim; j++) {
-                firefly.centerVector[i](j) = (1 - beta) * firefly.centerVector[i](j) + beta * destinationFirefly.centerVector[i](j) + sigma * (score(mt) - 0.5);
+                firefly.centerVector[i](j) = (1 - beta) * firefly.centerVector[i](j) + beta * destinationFirefly.centerVector[i](j) + alpha * (score(mt) - 0.5) * 2;
             }
         }
         
         for (int i = 0; i < dim; i++) {
-            firefly.biases[i] = (1 - beta) * firefly.biases[i] + beta * destinationFirefly.biases[i] + sigma * (score(mt) - 0.5);
+            firefly.biases[i] = (1 - beta) * firefly.biases[i] + beta * destinationFirefly.biases[i] + alpha * (score(mt) - 0.5) * 2;
         }
     }
     
     //ランダムに移動　t <- t + sigma*(rand-1/2)
-    void randomlyWalk(Firefly &firefly , double sigma) {
-        for (int i = 0; i < rbfCount; i++) {
-            for (int j = 0; j < dim; j++) {
-                firefly.weights(i, j) += sigma * (score(mt) - 0.5);
-            }
-            
-            firefly.spreads[i] += sigma * (score(mt) - 0.5);
-            
-            for (int j = 0; j < dim; j++) {
-                firefly.centerVector[i](j) += sigma * (score(mt) - 0.5);
-            }
+    void randomlyWalk(Firefly &firefly) {
+        firefly.weights = Eigen::MatrixXd::Random(rbfCount, dim);
+        
+        auto spreadsIter = firefly.spreads.begin();
+        while (spreadsIter != firefly.spreads.end()) {
+            *spreadsIter = score(mt);
+            spreadsIter++;
         }
         
-        for (int i = 0; i < dim; i++) {
-            firefly.biases[i] += sigma * (score(mt) - 0.5);
+        auto centerVectorIter = firefly.centerVector.begin();
+        while (centerVectorIter != firefly.centerVector.end()) {
+            *centerVectorIter = Eigen::VectorXd::Random(dim);
+            centerVectorIter++;
         }
+        
+        auto biasesIter = firefly.biases.begin();
+        while (biasesIter != firefly.biases.end()) {
+            *biasesIter = score(mt);
+            biasesIter++;
+        }
+        
+//        for (int i = 0; i < rbfCount; i++) {
+//            for (int j = 0; j < dim; j++) {
+//                firefly.weights(i, j) += alpha * (score(mt) - 0.5) * 2;
+//            }
+//            
+//            firefly.spreads[i] += alpha * (score(mt) - 0.5) * 2;
+//            
+//            for (int j = 0; j < dim; j++) {
+//                firefly.centerVector[i](j) += alpha * (score(mt) - 0.5) * 2;
+//            }
+//        }
+//        
+//        for (int i = 0; i < dim; i++) {
+//            firefly.biases[i] += alpha * (score(mt) - 0.5) * 2;
+//        }
     }
     
     //一番いい結果のindexで出力
     Eigen::VectorXd output(const Eigen::VectorXd &input) const {
-        return fireflies[bestFireflyIndex].output(input);
+        return fireflies[0].output(input);
+    }
+    
+    void outputBestFirefly(std::ofstream &output) {
+        Firefly bestFirefly = fireflies[0];
+        
+        output << "double weights[][" << dim << "] = {";
+        long rows = bestFirefly.weights.rows();
+        long cols = bestFirefly.weights.cols();
+        for (int i = 0; i < rows; i++) {
+            output << "{";
+            for (int j = 0; j < cols; j++) {
+                output << bestFirefly.weights(i, j);
+                if (j < cols - 1) {
+                    output << ", ";
+                }
+            }
+            if (i == rows - 1) {
+                output << "}";
+            }
+            else {
+                output << "}, ";
+            }
+        }
+        output << "};" << std::endl;
+        
+        output << "double centerVector[][" << dim << "] = {";
+        auto centerVectorIter = bestFirefly.centerVector.begin();
+        while (centerVectorIter != bestFirefly.centerVector.end()) {
+            output << "{";
+            for (int i = 0; i < (*centerVectorIter).size(); i++) {
+                output << (*centerVectorIter)[i];
+                if (i < (*centerVectorIter).size() - 1) {
+                    output << ", ";
+                }
+            }
+            output << "}";
+            centerVectorIter++;
+            if (centerVectorIter != bestFirefly.centerVector.end()) {
+                output << ", ";
+            }
+        }
+        output << "};" << std::endl;
+        
+        output << "double spreads[] = {";
+        auto spreadsIter = bestFirefly.spreads.begin();
+        while (spreadsIter != bestFirefly.spreads.end()) {
+            output << *spreadsIter;
+            spreadsIter++;
+            if (spreadsIter != bestFirefly.spreads.end()) {
+                output << ", ";
+            }
+        }
+        output << "};" << std::endl;
+        
+        output << "double biases[] = {";
+        auto biasesIter = bestFirefly.biases.begin();
+        while (biasesIter != bestFirefly.biases.end()) {
+            output << *biasesIter;
+            biasesIter++;
+            if (biasesIter != bestFirefly.biases.end()) {
+                output << ", ";
+            }
+        }
+        output << "};" << std::endl;
     }
     
     void displayFirefly(int index) const {
--- FireflyProject/main.cpp
+++ FireflyProject/main.cpp
@@ -6,12 +6,16 @@
 //  Copyright (c) 2013年 Keisuke Karijuku. All rights reserved.
 //
 
+//many thanks for your kind attention.
+
+
 #include <iostream>
 #include <fstream>
 #include "Eigen/Dense"
 #include "FireflyRBFTraining.h"
 #include "Wave.h"
 #include "analysis.h"
+
 
 double LogisticMap(double x)
 {
@@ -23,42 +27,32 @@ int main(int argc, char *argv[])
     using namespace std;
     using namespace Eigen;
     
-    int dim = 1;
-    int dataCount = 1200;
+    int dim = 5;
+    int dataCount = 100000;
     int offset = 7;
-    int rbfCount = 100;
+    int rbfCount = 200;
     int fireflyCount = 500;
-    int iteration = 50;
-    
+    int maxGeneration = 2000;
     int startDataCount = 100000;
     
-    //many thanks for your kind attention.
-    
     vector<double> tmp;
-	vector<int> fp;
+//	vector<int> fp;
 	Wave wav;
 	if(wav.InputWave("sample.wav") != 0)
 		return -1;
 	wav.StereoToMono();
 	wav.GetData(tmp);
 //	GetFlucPeriod(fp, tmp);
+//    std::cout << fp.size() << std::endl;
+    
 //    double max = 0, min = 99999999;
-//	for (int i = 0; i < dataCount + offset; i++) {
-//		if (max < fp[i]) {
-//			max = fp[i];
-//		}
-//		if (min > fp[i]) {
-//			min = fp[i];
-//		}
-//	}
-//    double max = 0, min = 99999999;
-//	for (int i = 0; i < dataCount + offset; i++) {
-//		if (max < tmp[i]) max = fp[i];
-//		if (min > tmp[i]) min = fp[i];
+//	for (int i = startDataCount; i < dataCount + offset; i++) {
+//		if (max < fp[i]) max = fp[i];
+//		if (min > fp[i]) min = fp[i];
 //	}
 //	std::vector<double> ffp(fp.size());
-//	for (int i = 0; i < dataCount + offset; i++) {
-//		ffp[i] = (fp[i] - min) / (max - min);
+//	for (int i = startDataCount; i < dataCount + offset; i++) {
+//		ffp[i] = ((double)fp[i] - min) / (max - min);
 //	}
     
     vector<VectorXd> input;
@@ -68,73 +62,55 @@ int main(int argc, char *argv[])
 //    mt19937 mt(random());
 //    uniform_real_distribution<double> score(0.0, dataCount - dim * offset);
     
-//    VectorXd xv(dim), yv(dim);
-//    for (int i = 0; i < dataCount; i++) {
-////        int rand = score(mt);
-//		for (int j = 0; j < dim; j++) {
-//			xv[j] = tmp[startDataCount + i + j * offset];
-//		}
-//		input.push_back(xv);
-//        
-//		for (int j = 0; j < dim; j++) {
-//			yv[j] = tmp[startDataCount + i + j * offset + 1];
-//		}
-//		output.push_back(yv);
-//	}
-    
-//    for (int i = 0; i < dataCount; i++) {
-//		Eigen::VectorXd xv(dim), yv(dim);
-//		for (int j = 0; j < dim; j++) {
-//			xv[j] = ffp[i + j * offset];
-//		}
-//		input.push_back(xv);
-//        
-//		for (int j = 0; j < dim; j++) {
-//			yv[j] = ffp[i + j * offset + 1];
-//		}
-//		output.push_back(yv);
-//	}
-    
-    //ロジスティック写像を使うとき
-    double x = 0.0001;
-    Eigen::VectorXd tmpVector(1);
-    tmpVector(0) = x;
-    input.push_back(tmpVector);
+    VectorXd xv(dim), yv(dim);
     for (int i = 0; i < dataCount; i++) {
-        x = LogisticMap(x);
-        tmpVector(0) = x;
-        if (i < dataCount - 1) {
-            input.push_back(tmpVector);
-        }
-        output.push_back(tmpVector);
-    }
+//        int rand = score(mt);
+		for (int j = 0; j < dim; j++) xv[j] = tmp[startDataCount + i + j * offset];
+		input.push_back(xv);
+        
+		for (int j = 0; j < dim; j++) yv[j] = tmp[startDataCount + i + j * offset + 1];
+		output.push_back(yv);
+	}
     
-    FireflyRBFTraining fireflyRBF(dim, dataCount, rbfCount, fireflyCount, 1.0, 0.1, iteration);
+//    //ロジスティック写像を使うとき
+//    double x = 0.01;
+//    Eigen::VectorXd tmpVector(1);
+//    tmpVector(0) = x;
+//    input.push_back(tmpVector);
+//    for (int i = 0; i < dataCount; i++) {
+//        x = LogisticMap(x);
+//        tmpVector(0) = x;
+//        if (i < dataCount - 1) {
+//            input.push_back(tmpVector);
+//        }
+//        output.push_back(tmpVector);
+//    }
     
+    FireflyRBFTraining fireflyRBF(dim, dataCount, rbfCount, fireflyCount, 1.0, 0.1, maxGeneration);
     fireflyRBF.training(input, output);
+    
+    ofstream fireflyOfs("bestFirefly.txt");
+    fireflyRBF.outputBestFirefly(fireflyOfs);
+    fireflyOfs.close();
     
     ofstream ofs("temp.txt");
     
-//    for (int i = 0; i < 100; i++) {
-//        ofs << i << " " << fp[i] << std::endl;
-//    }
-    
-//    VectorXd tmpInput(dim);
-//    for (int i = 0; i < dim; i++) {
-//        tmpInput(i) = input[0](i);
-//    }
-//    for (int i = 0; i < 1000; i++) {
-//        ofs << i << " " << output[i](0) << " ";
-////        tmpInput = fireflyRBF.output(tmpInput);
-////        ofs << tmpInput(0);
-//        ofs << endl;
-//    };
-    
-    for (double x = 0; x < 1.0; x += 0.01) {
-        Eigen::VectorXd tmpVector(1);
-        tmpVector[0] = x;
-        ofs << x << " " << LogisticMap(x) << " " << fireflyRBF.output(tmpVector) << std::endl;
+    VectorXd tmpInput(dim);
+    for (int i = 0; i < dim; i++) {
+        tmpInput(i) = input[0](i);
     }
+    for (int i = 0; i < 100; i++) {
+        ofs << i << " " << output[i](0) << " ";
+        tmpInput = fireflyRBF.output(tmpInput);
+        ofs << tmpInput(0);
+        ofs << endl;
+    };
+    
+//    //ロジスティック写像を使うとき
+//    for (double x = 0; x < 1.0; x += 0.01) {
+//        tmpVector[0] = x;
+//        ofs << x << " " << LogisticMap(x) << " " << fireflyRBF.output(tmpVector) << std::endl;
+//    }
     
     ofs.close();
     
